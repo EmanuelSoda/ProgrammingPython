540d95cef4c1c48b1df037579a0c7024a30a39d0
Emanuel <emanuelsoda@gmail.com>
2020-08-10 19:12:13 +0200

    Dovrebbe funzionare tutto...

---
+++ Burrows_Wheeler_Transform/main.py
@@ -0,0 +1,61 @@
+import numpy as np
+
+class not_a_string_error(TypeError):
+    pass
+class invalid_string_error(Exception):
+    pass
+
+
+
+
+''' Burrow - Wheeler Transform (BWT)
+    input: a string with or without a termination character not in the alphabet
+    output: the BWT of the input string '''
+def transform(string):
+    # check if the input is a string
+    if type(string) != str:
+        raise not_a_string_error(string)
+    # the string must contains only one termination char
+    elif string.count('$') > 1:
+        raise invalid_string_error(string)
+
+    # check if the last char contains the '$' otherwhise add it
+    elif string[-1] != '$':
+        string += '$'
+
+    string_size = len(string)
+
+    matrix = np.sort(np.array([string[-i :] + string[: -i] for i in np.arange(string_size)], dtype= '<U' + str(string_size)))
+    # the final string is the concatenation of the last char of all the rows of the rotation matrix
+    return str("".join([matrix[i][-1] for i in np.arange(string_size)]))
+
+
+
+
+
+
+''' Inverse of the Burrow - Wheeler Transform (BWT)
+    input: the BWT of a string
+    output: the original string'''
+def inverse(bwt):
+    if type(bwt) != str:
+        raise not_a_string_error(bwt)
+    elif bwt.count('$') != 1:
+        raise invalid_string_error(bwt)
+
+    n = len(bwt)
+    bwt_array = np.array(list(bwt))
+    firstCol = np.sort(bwt_array)
+
+    finalString = ''
+    i = 0
+    while len(finalString) != n:
+        item = bwt_array[i]
+        # the position is given by the index of the first occurrence in the first column of the rotation matrix plus the number of times that the item is present int the bwt till that moment
+        itemindex = np.where(firstCol == item)[0][0] +  np.count_nonzero(bwt_array[: i] == item)
+        # append the character to the top of the string
+        finalString = firstCol[itemindex] + finalString
+        i = itemindex
+
+    # return the string without the '$'
+    return finalString[1: ]
--- prova.py
+++ prova.py
@@ -1,18 +1,12 @@
-
-def suffixArray(s):
-    setups = sorted([(s[i : ], i) for i in range(0, len(s))])
-    print(setups, '\n\n')
-    return map(lambda x: x[1], setups)
-
-def bwtViaSa(t):
-    bw = []
-    for si in suffixArray(t):
-        if si == 0:
-            bw.append('$')
-        else:
-            bw.append(t[si-1])
-
-    return ''.join(bw)
+from Burrows_Wheeler_Transform import transform
+from Burrows_Wheeler_Transform import inverse
 
 
-print(bwtViaSa("Tomorrow_and_tomorrow_and_tomorrowTomorrow"))
+milleCaratteri =  'acctgacctgacacctgacctgactgaccctgaccctgaccctgacccctgactgacccctgacctgacctgacctgacctgacctgacctgacctgacctgactgaccctgaccctgaccctgacccctgactgacccctgacctgacctgacctgacctgacctgacctgacctgacctgactgaccctgaccctgaccctgacccctgactgacccctgacctgacctgacctgacctgacctgacctgacctgacctgactgaccctgaccctgaccctgacccctgactgacccctgacctgacctgacctgacctgacctgacctgacctgacctgactgaccctgaccctgaccctgacccctgactgacccctgacctgacctgacctgacctgacctgacctgacctgacctgactgaccctgaccctgaccctgacccctgactgacccctgacctgacctgacctgacctgacctgacctgacctgacctgactgaccctgaccctgaccctgacccctgactgacccctgacctgacctgacctgacctgacctgacctgtgaccctgaacctgacctgacacctgacctgactgaccctgaccctgaccctgacccctgactgacccctgacctgacctgacctgacctgacctgacctgacctgacctgactgaccctgaccctgaccctgacccctgactgacccctgacctgacctgacctgacctgacctgacctgacctgacctgactgaccctgaccctgaccctgacccctgactgacccctgacctgacctgacctgacctgacctgacctgacctgacctgactgaccctgaccctgaccctgacccctgactgacccctgacctgacctgacctgacctgacctgacctgacctgacctgactgaccctgaccctgaccctgacccctgactgacccctgacctgacctgacctgacctgacctgacctgacctgac'
+
+stringa = "Tomorrow_and_tomorrow_and_tomorrowTomorrow"
+
+trasformata = transform(milleCaratteri)
+print(trasformata)
+inversa = inverse(trasformata)
+print(inversa)
