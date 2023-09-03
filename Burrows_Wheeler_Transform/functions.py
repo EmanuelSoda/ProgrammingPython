4b34f2d8bb3f5309c0886011835396cdab7acb5e
Emanuel Soda <emanuelsoda@gmail.com>
2023-09-03 20:08:25 +0200

    Update functions.py
    Added check_input since the input check is identical for both function

--- Burrows_Wheeler_Transform/functions.py
+++ Burrows_Wheeler_Transform/functions.py
@@ -5,45 +5,48 @@ class not_a_string_error(TypeError):
 class invalid_string_error(Exception):
     pass
 
-
-def transform(string):
+def check_input(string) -> str:
     """
-    Burrow - Wheeler Transform (BWT)
+    check_input
     input: a string with or without a termination character not in the alphabet
-    output: the BWT of the input string """
-
-    # check if the input is a string
+    output: the string containing the termination character
+    """
     if type(string) != str:
         raise not_a_string_error('The input ' + string + ' is not a string ')
-    # the string must contains only one termination char
+    # The string must contain only one termination char
     elif string.count('$') > 1:
         raise invalid_string_error('The input ' + string + ' cointains more than one $' )
 
-    # check if the last char contains the '$' otherwhise add it
+    # check if the last char contains the '$' otherwise add it
     elif string[-1] != '$':
         string += '$'
+
+    return string
+
+def transform(string) -> str:
+    """
+    Burrow - Wheeler Transform (BWT)
+    input: a string with or without a termination character not in the alphabet
+    output: the BWT of the input string 
+    """
+
+    # Check if the input is a string
+    string = check_input(string)
 
     string_size = len(string)
 
     matrix = np.sort(np.array([string[-i :] + string[: -i] for i in np.arange(string_size)], dtype= '<U' + str(string_size)))
-    # the final string is the concatenation of the last char of all the rows of the rotation matrix
+    # The final string is the concatenation of the last char of all the rows of the rotation matrix
     return str("".join([matrix[i][-1] for i in np.arange(string_size)]))
 
-
-
-
-
-
-def inverse(bwt):
+def inverse(bwt) -> str:
     '''
-    Inverse of the Burrow - Wheeler Transform (BWT)
+    The inverse of the Burrow - Wheeler Transform (BWT)
     input: the BWT of a string
     output: the original string '''
 
-    if type(bwt) != str:
-        raise not_a_string_error('The input ' + bwt + ' is not a string')
-    elif bwt.count('$') != 1:
-        raise invalid_string_error('The input ' + bwt + ' does not contain exactly one $ character')
+    # Check if the input is a string
+    bwt = check_input(bwt)
 
     n = len(bwt)
     bwt_array = np.array(list(bwt))
@@ -53,11 +56,14 @@ def inverse(bwt):
     i = 0
     while len(finalString) != n:
         item = bwt_array[i]
-        # the position is given by the index of the first occurrence in the first column of the rotation matrix plus the number of times that the item is present int the bwt till that moment
-        itemindex = np.where(firstCol == item)[0][0] +  np.count_nonzero(bwt_array[: i] == item)
-        # append the character to the top of the string
+        
+        # The position is given by the index of the first occurrence in the first column of the rotation 
+        # matrix plus the number of times that the item is present in the bwt till that moment
+        itemindex = np.where(firstCol == item)[0][0] + np.count_nonzero(bwt_array[: i] == item)
+        
+        # Append the character to the top of the string
         finalString = firstCol[itemindex] + finalString
         i = itemindex
 
-    # return the string without the '$'
+    # Returing the string without the '$'
     return finalString[1:]
